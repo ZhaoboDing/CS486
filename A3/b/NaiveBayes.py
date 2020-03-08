1a44b626c48685e4a720dfa5bbec6147eb768261
BananaWolfDing <ding.zb@yahoo.com>
2020-03-07 22:40:02 -0500

    modify probability implementation

--- A3/b/NaiveBayes.py
+++ A3/b/NaiveBayes.py
@@ -1,21 +1,26 @@
+import numpy as np
 from collections import defaultdict
 
 
 class NaiveBayes:
     def __init__(self, word_map):
         self.word_set = set(word_map)
-        self.prob = defaultdict(lambda: defaultdict(lambda: 0))
 
     def fit(self, docs, labels):
-        stat = defaultdict(lambda: defaultdict(lambda: 0))
+        num_words = len(self.word_set)
+        num_label = len(labels)
+        stat = np.zeros((num_words, num_label))
+
         for document in docs:
             for word in document.word_list:
-                stat[document.label][word] += 1
+                stat[word][document.label] += 1
 
-        for label in labels:
-            total = sum(stat[label].values())
-            for word in stat[label]:
-                self.prob[label][word] = (stat[label][word] + 1) / (total + len(labels))
+        label_sum = np.sum(stat, axis=1)
+        word_sum = np.sum(stat, axis=0)
+        # prob_given_label[word][label] stores the value of Pr(word | label)
+        self.prob_given_label = (stat + 1) / (label_sum + num_words)
+        # prob_given_word[label][word] stores the value of Pr(label | word)
+        self.prob_given_word = np.transpose((stat + 1) / (word_sum + num_label))
 
     def predict(self, word_list, poss_labels):
         p = defaultdict(str)
@@ -23,8 +28,8 @@ class NaiveBayes:
             p[label] = 1
             for word in self.word_set:
                 if word in word_list:
-                    p[label] *= self.prob[label][word]
+                    p[label] *= self.prob_given_label[word][label]
                 else:
-                    p[label] *= 1 - self.prob[label][word]
+                    p[label] *= 1 - self.prob_given_label[word][label]
 
         return max(p, key=p.get)
